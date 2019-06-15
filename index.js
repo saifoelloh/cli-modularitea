9d8a175014da4cd691f7be1a1344c572c3f31870
saifoelloh <saifoelloh@gmail.com>
2019-06-15 10:20:32 +0700

    first step to install web module

--- index.js
+++ index.js
@@ -1,21 +1,31 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { exec } = require('child_process');
+const appList = require('./list.json');
 
 const app = express();
 
 app
 .use(bodyParser.json())
-.use('/api/web', function(req, res) {
-    exec('gksudo "apt install apache2 -y"', function(error, stdout, stderr){
-        console.log(error, stderr, stdout);
+.use('/api/web', async function(req, res) {
+    const cc = await exec('wget -P ~/ https://az764295.vo.msecnd.net/stable/c7d83e57cd18f18026a8162d042843bda1bcf21f/code_1.35.1-1560350270_amd64.deb', async function(error, stderr, stdout){
         if (error) {
             return res.status(500).json({
                 message: 'Internal server error',
                 error: error
             });
         }
-        res.status(200).json({stderr, stdout});
+        const xampp = await exec('wget -P ~ https://images.unsplash.com/photo-1560507074-b9eb43faab00?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&dl=pawel-czerwinski-1678552-unsplash.jpg', function(error, stderr, stdout){
+            console.log(`stderr: ${stderr}\nstdout: ${stdout}\nerror: ${error}`);
+            if (error) {
+                return res.status(500).json({
+                    message: 'Internal server error',
+                    error: error
+                });
+            }
+            return {stderr,stdout};
+        })
+        res.status(200).json(`stderr: ${stderr}\nstdout: ${stdout}\nxampp: ${xampp}`);
     });
 })
 .use('/api/multimed', function(req, res){
